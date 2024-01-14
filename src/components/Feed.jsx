4db43dde2dfa20edb4821f2af6d86bdbff22bd68
Ahsan <ahsaninfo1@gmail.com>
2024-01-14 18:48:52 +0530

    Add Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -20,7 +20,7 @@ const Feed = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
 
-          {/* {!loading && searchResults.map((item) => {
+          {!loading && searchResults.map((item) => {
 
             if(item.type !== "video") return false;
 
@@ -32,7 +32,7 @@ const Feed = () => {
             );
 
           })}
-           */}
+          
            <h1>Error</h1>
         </div>
       </div>
