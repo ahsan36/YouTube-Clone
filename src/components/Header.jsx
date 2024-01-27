b447f3dcf250a2e04f8c398a500d133110b1d607
Ahsan <ahsaninfo1@gmail.com>
2024-01-27 06:32:24 +0530

    Final commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -116,7 +116,7 @@ const Header = () => {
         </div>
 
         <div className="flex h-8 w-8 mt-1 overflow-hidden rounded-full md:ml-4">
-            <img src="./src/images/profile-pic.png" />
+            <img src="xsgames.co/randomusers/avatar.php?g=male" />
         </div>
 
       </div>
